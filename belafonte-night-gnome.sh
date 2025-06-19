b908e11d62c528f942fe530e1fc46d4546ecf7d2
Rahul Krishna <i.m.ralk@gmail.com>
2025-06-19 11:34:00 -0400

    Add belafonte night gnome theme
    Signed-off-by: Rahul Krishna <i.m.ralk@gmail.com>

--- belafonte-night-gnome.sh
+++ belafonte-night-gnome.sh
@@ -4,7 +4,7 @@ DCONF_BASE=/org/gnome/terminal/legacy/profiles:
 DEFAULT_SLUG=$(dconf read $DCONF_BASE/default | tr -d \')
 
 dconf write $DCONF_BASE/:$DEFAULT_SLUG/use-theme-colors "false"
-dconf write $DCONF_BASE/:$DEFAULT_SLUG/background-color "'#20111B'"
+dconf write $DCONF_BASE/:$DEFAULT_SLUG/background-color "'#1A0E16'"
 dconf write $DCONF_BASE/:$DEFAULT_SLUG/foreground-color "'#D5CCBA'"
 dconf write $DCONF_BASE/:$DEFAULT_SLUG/cursor-background-color "'#968C83'"
 dconf write $DCONF_BASE/:$DEFAULT_SLUG/cursor-foreground-color "'#20111B'"
